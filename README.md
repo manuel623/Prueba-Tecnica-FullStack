6b0967e7baf51c26e3af143e8398f5de39982064
manuel623 <manucardona2005@gmail.com>
2024-10-02 19:00:36 -0500

    :memo: se agrega  archivo README.md en la raiz del repositorio

---
+++ README.md
@@ -0,0 +1,18 @@
+### Project TaskManagement
+
+### Project Description
+
+Project to be able to manage tasks
+
+### Technologies used
+
+React
+Axios
+SQL Server Express
+.NET 8
+
+### Ports used
+
+Frontend: 3000
+Backend: 5150 
+
--- TaskManagementApi/obj/Debug/net8.0/TaskManagementApi.AssemblyInfo.cs
+++ TaskManagementApi/obj/Debug/net8.0/TaskManagementApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagementApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5eb9c5060d53f38420a8c585f932b9a6471f15b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagementApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagementApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
